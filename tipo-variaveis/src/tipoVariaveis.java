22089179c009f40ce8e975bf23be96bb480f8f97
Bikulcius <bikulciusluis@gmail.com>
2025-02-01 18:24:22 -0300

    codigo usando int

--- tipo-variaveis/src/tipoVariaveis.java
+++ tipo-variaveis/src/tipoVariaveis.java
@@ -1,5 +1,6 @@
 public class tipoVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+     int salario =3000;
+        System.out.println(salario);
     }
 }
